6df1c62f2786d2ce748970198a57f5c945c2af1f
Nguyễn Đức Long <nguyenduclong.ict@gmail.com>
2021-03-13 16:58:49 +0700

    fix cli

--- cli/index.js
+++ cli/index.js
@@ -5,14 +5,11 @@ const path = require("path");
 const ejs = require("ejs");
 
 const program = new Command();
-program
-  .option("-b, --base <type>", "Base path, default src/services")
-  .command("entity <dir> <name>")
-  .action((dir, name) => {
-    const basePath = path.join(process.cwd(), program.base || "src/services");
-    const entityPath = path.join(basePath, dir, name);
-    createEntity(entityPath, name);
-  });
+program.command("entity <dir> <name>").action((dir, name) => {
+  const basePath = process.cwd();
+  const entityPath = path.join(basePath, dir, name);
+  createEntity(entityPath, name);
+});
 
 program.parse(process.argv);
 
